c5fda8ba5882ab70484022fb3546804859eefafa
xejwofuxu <1968643693@qq.com>
2022-07-06 11:14:26 +0800

    update

--- osa-data-cs/src/main/java/com/siemens/osa/data/cs/DataCSApplication.java
+++ osa-data-cs/src/main/java/com/siemens/osa/data/cs/DataCSApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataApplication {
+public class DataCSApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DataApplication.class, args);
+        SpringApplication.run(DataCSApplication.class, args);
     }
 }
