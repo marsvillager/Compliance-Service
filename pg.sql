f94572eb23e16056e92ef64c7d60ea4fb342009b
xejwofuxu <1968643693@qq.com>
2022-07-09 13:49:29 +0800

    format

--- pg.sql
+++ pg.sql
@@ -174,12 +174,12 @@ COMMENT ON COLUMN public.result.status IS '比对的结果（pass/failed）';
 --
 
 COPY public.config ("timestamp", id, os, rule_id, data, type, param) FROM stdin;
-2022-07-05 17:40:27.19112+08	2	windows10	BL696_0461	{0}	0	{"Xbox Live 网络服务"}
 2022-07-05 17:56:24.686603+08	2	windows10	BL696_0086	{1}	0	{"Machine\\\\System\\\\CurrentControlSet\\\\Control\\\\SCMConfig:EnableSvchostMitigationPolicy"}
 2022-07-05 17:56:35.588643+08	2	windows10	BL696-0711	{0}	0	{"Machine\\\\Software\\\\Policies\\\\Microsoft\\\\Windows\\\\PowerShell:EnableScripts","Machine\\\\Software\\\\Policies\\\\Microsoft\\\\Windows\\\\PowerShell:ExecutionPolicy"}
 2022-07-05 17:56:48.169855+08	2	debain10	BL999_8949	{masked}	0	{"systemctl is-enabled ctrl-alt-del.target"}
 2022-07-05 17:57:27.191144+08	2	debain10	BL999_7387	{"fs.protected_symlinks = 1","fs.protected_hardlinks = 1"}	0	{"sysctl fs.protected_symlinks","sysctl fs.protected_hardlinks"}
 2022-07-05 17:57:31.251354+08	2	debain10	BL999_6629	{0664,0,0}	0	{"stat /etc/issue"}
+2022-07-05 17:40:27.19112+08	2	windows10	BL696_0461	{0}	0	{"Xbox Live 网络服务"}
 \.
 
 
@@ -188,7 +188,7 @@ COPY public.config ("timestamp", id, os, rule_id, data, type, param) FROM stdin;
 --
 
 COPY public.result ("timestamp", id, os, server_ip, host_ip, rule_id, expected, actual, status) FROM stdin;
-2022-07-05 17:40:27.19112+08	2	windows10	192.168.1.155	192.168.1.150	BL696_0461	{1}	{1}	pass
+2022-07-05 17:40:27.19112+08	2	windows10	192.168.1.155	192.168.1.150	BL696_0461	{0}	{0}	failed
 \.
 
 
